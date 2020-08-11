6ac7cb6cb25ef8c30007ce717d4d29563501edb3
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-08-11 23:41:11 +0200

    add button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,8 @@ const orderSummary = (props) => {
       <ul>{ingredientSummary}</ul>
 
       <p>Continue to checkout</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   );
 };
